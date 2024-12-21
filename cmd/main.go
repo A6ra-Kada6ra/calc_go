7fcd9e676dcfe4da9095d7d3b511a1a77d47e428
A6ra-Kada6ra <magishenn@yandex.ru>
2024-12-21 19:35:10 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-    "log"
-    "net/http"
-
-    "github.com/pashapdev/calc_go/internal/application"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/pashapdev/calc_go/pkg/calculation"
 )
 
+type Request struct {
+	Expression string `json:"expression"`
+}
+
+type Response struct {
+	Result string `json:"result,omitempty"`
+	Error  string `json:"error,omitempty"`
+}
+
+func CalcHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, `{"error": "Method not allowed"}`, http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req Request
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil || req.Expression == "" {
+		http.Error(w, `{"error": "Internal server error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	result, err := calculation.Calc(req.Expression)
+	if err != nil {
+		http.Error(w, `{"error": "Expression is not valid"}`, http.StatusUnprocessableEntity)
+		return
+	}
+
+	response := Response{Result: result}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
-    app := application.New()
-    if err := app.RunServer(); err != nil {
-        log.Fatalf("Could not start server: %s\n", err.Error())
-    }
+	http.HandleFunc("/api/v1/calculate", CalcHandler)
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
